559f986181d46a32be1b2887c063baf6c5d56311
noambo <nbaroz@gmail.com>
2014-09-11 11:12:16 +0300

    added sound to push notification

--- android/BeautyApp/trunk/src/com/pictureit/noambaroz/beautyapp/gcm/GcmIntentService.java
+++ android/BeautyApp/trunk/src/com/pictureit/noambaroz/beautyapp/gcm/GcmIntentService.java
@@ -6,6 +6,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 
@@ -34,8 +36,10 @@ public class GcmIntentService extends IntentService {
 
 		if (!extras.isEmpty()) { // has effect of unparcelling Bundle
 			/*
-			 * Filter messages based on message type. Since it is likely that GCM will be extended in the future with new message types, just ignore any message types you're not
-			 * interested in, or that you don't recognize.
+			 * Filter messages based on message type. Since it is likely that
+			 * GCM will be extended in the future with new message types, just
+			 * ignore any message types you're not interested in, or that you
+			 * don't recognize.
 			 */
 			if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
 				// sendNotification("Send error: " + extras.toString(), extras);
@@ -77,9 +81,10 @@ public class GcmIntentService extends IntentService {
 		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
 		// Notification.FLAG_ONGOING_EVENT | Notification.FLAG_NO_CLEAR);
 
+		Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this).setSmallIcon(R.drawable.ic_stat_gcm)
 				.setContentTitle("GCM Notification").setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
-				.setContentText(msg).setAutoCancel(true);
+				.setContentText(msg).setAutoCancel(true).setSound(uri);
 
 		mBuilder.setContentIntent(contentIntent);
 		mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
