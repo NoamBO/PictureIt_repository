9310110d3084864fe56ed9d0d005fb719947b0ee
noambo <nbaroz@gmail.com>
2014-10-19 16:53:56 +0300

    +Message Activity

--- android/BeautyApp/trunk/src/com/pictureit/noambaroz/beautyapp/ActivityMessagesInner.java
+++ android/BeautyApp/trunk/src/com/pictureit/noambaroz/beautyapp/ActivityMessagesInner.java
@@ -3,9 +3,17 @@ package com.pictureit.noambaroz.beautyapp;
 import android.app.Fragment;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.pictureit.noambaroz.beautyapp.data.Constant;
 import com.pictureit.noambaroz.beautyapp.data.DataProvider;
+import com.pictureit.noambaroz.beautyapp.server.ImageLoaderUtil;
 
 public class ActivityMessagesInner extends ActivityWithFragment {
 
@@ -41,6 +49,9 @@ public class ActivityMessagesInner extends ActivityWithFragment {
 		String price;
 		String phone;
 
+		Button bConfirm;
+		Button bReject;
+
 		@Override
 		public void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
@@ -64,6 +75,33 @@ public class ActivityMessagesInner extends ActivityWithFragment {
 				return true;
 			} else
 				return false;
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			View v = inflater.inflate(R.layout.fragment_message, container, false);
+			ImageView pic = findView(v, R.id.iv_message_beautician_pic);
+			TextView name = findView(v, R.id.tv_message_beautician_name);
+			TextView address = findView(v, R.id.tv_message_address);
+			TextView raters = findView(v, R.id.tv_message_raters);
+			RatingBar ratingBar = findView(v, R.id.rb__message_rating);
+			TextView date = findView(v, R.id.tv_message_inner_treatment_date);
+			TextView location = findView(v, R.id.tv_message_inner_treatment_location);
+			TextView remarks = findView(v, R.id.tv_message_inner_remarks);
+			TextView price = findView(v, R.id.tv_message_inner_price);
+			bConfirm = findView(v, R.id.b_message_inner_confirm);
+			bReject = findView(v, R.id.b_message_inner_reject);
+
+			name.setText(beauticianName);
+			address.setText(beauticianAddress);
+			raters.setText("( " + beauticianRaters + " )");
+			ratingBar.setRating((float) beauticianRating);
+			date.setText(treatmentDate);
+			location.setText(treatmentLocation);
+			remarks.setText(this.remarks);
+			price.setText(this.price);
+			ImageLoaderUtil.display(picUrl, pic);
+			return v;
 		}
 	}
 }
--- android/BeautyApp/trunk/src/com/pictureit/noambaroz/beautyapp/server/ImageLoaderUtil.java
+++ android/BeautyApp/trunk/src/com/pictureit/noambaroz/beautyapp/server/ImageLoaderUtil.java
@@ -1,5 +1,7 @@
 package com.pictureit.noambaroz.beautyapp.server;
 
+import android.widget.ImageView;
+
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
 import com.pictureit.noambaroz.beautyapp.R;
@@ -11,5 +13,11 @@ public class ImageLoaderUtil {
 				.considerExifParams(false).displayer(new RoundedBitmapDisplayer(30))
 				.showImageForEmptyUri(R.drawable.avatar).showImageOnFail(R.drawable.avatar)
 				.showImageOnLoading(R.drawable.avatar).build();
+	}
+
+	public static void display(String uri, ImageView imageView) {
+		com.nostra13.universalimageloader.core.ImageLoader imageLoader = com.nostra13.universalimageloader.core.ImageLoader
+				.getInstance();
+		imageLoader.displayImage(uri, imageView, getDisplayImageOptions());
 	}
 }
