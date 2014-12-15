e092c8049646c30e301501e321b0b8bc0dfc8340
noambo <nbaroz@gmail.com>
2014-12-15 15:20:15 +0200

    bug fixed

--- android/BeautyApp/trunk/src/com/pictureit/noambaroz/beautyapp/FragmentReorder.java
+++ android/BeautyApp/trunk/src/com/pictureit/noambaroz/beautyapp/FragmentReorder.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 
+import com.pictureit.noambaroz.beautyapp.data.StringArrays;
 import com.pictureit.noambaroz.beautyapp.data.TreatmentSummary;
 import com.pictureit.noambaroz.beautyapp.data.TreatmentType;
 import com.pictureit.noambaroz.beautyapp.helper.ServiceOrderManager;
@@ -14,6 +15,14 @@ public class FragmentReorder extends FragmentServiceOrder {
 			String remarks, ArrayList<TreatmentType> treatments) {
 		ServiceOrderManager manager = new ServiceOrderManager(activity);
 		TreatmentSummary t = new TreatmentSummary();
+
+		for (int i = 0; i < treatments.size(); i++) {
+			TreatmentType tt = StringArrays.getTreatmentType(activity, treatments.get(i).getTreatments_id());
+			treatments.get(i).setDescription(tt.getDescription());
+			treatments.get(i).setName(tt.getName());
+			treatments.get(i).setPrice(tt.getPrice());
+		}
+
 		t.setForWho(forWho);
 		t.setRemarks(remarks);
 		t.setTretments(treatments);
