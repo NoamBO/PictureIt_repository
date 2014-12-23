35721db6a898ccc8cf349c37391a018670562b1e
noambo <nbaroz@gmail.com>
2014-12-23 14:01:39 +0200

    Main screen / Appling th design on switch component Main Activity / Monitoring user location Map fragment / Added map

--- android/BeautyApp/beautician-trunk/src/com/pictureit/noambaroz/beauticianapp/MainActivity.java
+++ android/BeautyApp/beautician-trunk/src/com/pictureit/noambaroz/beauticianapp/MainActivity.java
@@ -1,9 +1,15 @@
 package com.pictureit.noambaroz.beauticianapp;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
 import android.graphics.Typeface;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -17,14 +23,59 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.pictureit.noambaroz.beautycianapp.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
+
+	private static final int REQUEST_UPDATE_GOOGLE_PLAY_APK = 12;
+
+	private static final String STATE_RESOLVING_ERROR = "resolving_error";
+
+	private GoogleApiClient mGoogleApiClient;
+
+	// Request code to use when launching the resolution activity
+	private static final int REQUEST_RESOLVE_GOOGLE_CLIENT_ERROR = 1001;
+	// Unique tag for the error dialog fragment
+	private static final String DIALOG_ERROR = "dialog_error";
+	// Bool to track whether the app is already resolving an error
+	private boolean mResolvingError = false;
+
+	private LocationRequest mLocationRequest;
+
+	private boolean mRequestingLocationUpdates;
+
+	private Location mCurrentLocation;
+
+	private LocationListener mMapFragmentLocationListener;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		mResolvingError = savedInstanceState != null && savedInstanceState.getBoolean(STATE_RESOLVING_ERROR, false);
+
+		createLocationRequestIfNeeded();
+
+		if (googlePlayServicesAvailable()) {
+			initActivity();
+			resume();
+			// GcmUtil.get(getApplicationContext()).registerToGcm();
+		}
+
+	}
+
+	private void initActivity() {
 		setContentView(R.layout.activity_main);
+		MapFragment mapFragment = new MapFragment();
+		getFragmentManager().beginTransaction().add(R.id.main_screen_fragment_container, mapFragment).commit();
 	}
 
 	@Override
@@ -32,6 +83,7 @@ public class MainActivity extends Activity {
 		super.onResume();
 		ActivityWithFragment.addViewToTopOfActionBar(MainActivity.this);
 		getActionBar().setTitle("");
+		resume();
 	}
 
 	@Override
@@ -113,5 +165,187 @@ public class MainActivity extends Activity {
 		startActivity(intent);
 		finish();
 		overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+	}
+
+	private boolean googlePlayServicesAvailable() {
+		int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		if (resultCode != ConnectionResult.SUCCESS) {
+			if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+				GooglePlayServicesUtil.getErrorDialog(resultCode, this, REQUEST_UPDATE_GOOGLE_PLAY_APK,
+						new OnCancelListener() {
+
+							@Override
+							public void onCancel(DialogInterface dialog) {
+								finish();
+							}
+						}).show();
+			} else {
+				finish();
+			}
+			return false;
+		}
+		return true;
+	}
+
+	private void resume() {
+		if (googlePlayServicesAvailable()) {
+			buildGoogleApiClient();
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_UPDATE_GOOGLE_PLAY_APK) {
+			if (resultCode == RESULT_OK) {
+				initActivity();
+				resume();
+			} else {
+				finish();
+			}
+		} else if (requestCode == REQUEST_RESOLVE_GOOGLE_CLIENT_ERROR) {
+			mResolvingError = false;
+			if (resultCode == RESULT_OK) {
+				// Make sure the app is not already connected or attempting to
+				// connect
+				if (!mGoogleApiClient.isConnecting() && !mGoogleApiClient.isConnected()) {
+					mGoogleApiClient.connect();
+				}
+			}
+		}
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putBoolean(STATE_RESOLVING_ERROR, mResolvingError);
+	}
+
+	protected void buildGoogleApiClient() {
+		if (mGoogleApiClient == null) {
+			mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this)
+					.addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+		}
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		connectToGooglePlayServicesApi();
+	}
+
+	private void connectToGooglePlayServicesApi() {
+		if (!mResolvingError && !mGoogleApiClient.isConnecting() && !mGoogleApiClient.isConnected()) {
+			mGoogleApiClient.connect();
+		}
+	}
+
+	private void disconnectFromGooglePlayServicesApi() {
+		mGoogleApiClient.disconnect();
+		mRequestingLocationUpdates = false;
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		if (mResolvingError) {
+			// Already attempting to resolve an error.
+			return;
+		} else if (result.hasResolution()) {
+			try {
+				mResolvingError = true;
+				result.startResolutionForResult(this, REQUEST_RESOLVE_GOOGLE_CLIENT_ERROR);
+			} catch (SendIntentException e) {
+				// There was an error with the resolution intent. Try again.
+				mGoogleApiClient.connect();
+			}
+		} else {
+			// Show dialog using GooglePlayServicesUtil.getErrorDialog()
+			showErrorDialog(result.getErrorCode());
+			mResolvingError = true;
+		}
+	}
+
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		if (!mRequestingLocationUpdates) {
+			startLocationUpdates();
+		}
+	}
+
+	@Override
+	public void onConnectionSuspended(int arg0) {
+	}
+
+	// The rest of this code is all about building the error dialog
+
+	/* Creates a dialog for an error message */
+	private void showErrorDialog(int errorCode) {
+		// Create a fragment for the error dialog
+		ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
+		// Pass the error that should be displayed
+		Bundle args = new Bundle();
+		args.putInt(DIALOG_ERROR, errorCode);
+		dialogFragment.setArguments(args);
+		dialogFragment.show(getFragmentManager(), "errordialog");
+	}
+
+	/* Called from ErrorDialogFragment when the dialog is dismissed. */
+	public void onDialogDismissed() {
+		mResolvingError = false;
+	}
+
+	/* A fragment to display an error dialog */
+	public static class ErrorDialogFragment extends DialogFragment {
+		public ErrorDialogFragment() {
+		}
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			// Get the error code and retrieve the appropriate dialog
+			int errorCode = this.getArguments().getInt(DIALOG_ERROR);
+			return GooglePlayServicesUtil.getErrorDialog(errorCode, this.getActivity(),
+					REQUEST_RESOLVE_GOOGLE_CLIENT_ERROR);
+		}
+
+		@Override
+		public void onDismiss(DialogInterface dialog) {
+			((MainActivity) getActivity()).onDialogDismissed();
+		}
+	}
+
+	protected void createLocationRequestIfNeeded() {
+		int interval = 15 * 60 * 1000; // 15 Minutes
+		int fastestInterval = 10 * 60 * 1000; // 10 Minutes
+
+		if (mLocationRequest != null)
+			return;
+		mLocationRequest = new LocationRequest();
+		mLocationRequest.setInterval(interval);
+		mLocationRequest.setFastestInterval(fastestInterval);
+		mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+	}
+
+	protected void startLocationUpdates() {
+		LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+		mRequestingLocationUpdates = true;
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		if (location == null)
+			return;
+
+		mCurrentLocation = location;
+		updateLocationOnBackendInBackground();
+		if (mMapFragmentLocationListener != null)
+			mMapFragmentLocationListener.onLocationChanged(location);
+	}
+
+	private void updateLocationOnBackendInBackground() {
+		// TODO Auto-generated method stub
+		Log.i("Got location!");
+	}
+
+	public void setMapFragmentLocationListener(LocationListener l) {
+		mMapFragmentLocationListener = l;
 	}
 }
--- android/BeautyApp/beautician-trunk/src/com/pictureit/noambaroz/beauticianapp/MapFragment.java
+++ android/BeautyApp/beautician-trunk/src/com/pictureit/noambaroz/beauticianapp/MapFragment.java
@@ -1,7 +1,102 @@
 package com.pictureit.noambaroz.beauticianapp;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MapFragment extends Fragment {
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapLoadedCallback;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.LatLng;
+import com.pictureit.noambaroz.beauticianapp.location.LoadingMapIndicator;
+import com.pictureit.noambaroz.beautycianapp.R;
 
+public class MapFragment extends Fragment implements LocationListener, OnMapLoadedCallback {
+
+	private MapView mapView;
+	private GoogleMap mMap;
+
+	private static final float INITIAL_ZOOM = 17;
+
+	private static final float MIN_ZOOM_ALLOWED = INITIAL_ZOOM - 1;
+
+	private LoadingMapIndicator mLoadingMapIndicator;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mLoadingMapIndicator = new LoadingMapIndicator(getActivity());
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_map_container, container, false);
+		mLoadingMapIndicator.showMapLoadingIndicator();
+		mapView = (MapView) v.findViewById(R.id.mapview);
+		mapView.onCreate(savedInstanceState);
+
+		return v;
+	}
+
+	private void initMapIfNeeded(Location location) {
+		if (mMap == null) {
+			if (location == null) {
+				mLoadingMapIndicator.showNoMapIndicator();
+				return;
+			}
+			mMap = mapView.getMap();
+			mMap.setOnMapLoadedCallback(this);
+
+			LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+
+			mMap.getUiSettings().setRotateGesturesEnabled(false);
+			mMap.getUiSettings().setTiltGesturesEnabled(false);
+			mMap.getUiSettings().setZoomControlsEnabled(false);
+			MapsInitializer.initialize(this.getActivity());
+			CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(ll, INITIAL_ZOOM);
+			mMap.animateCamera(cameraUpdate);
+		}
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		((MainActivity) activity).setMapFragmentLocationListener(this);
+	}
+
+	@Override
+	public void onResume() {
+		mapView.onResume();
+		super.onResume();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		mapView.onDestroy();
+	}
+
+	@Override
+	public void onLowMemory() {
+		super.onLowMemory();
+		mapView.onLowMemory();
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		initMapIfNeeded(location);
+	}
+
+	@Override
+	public void onMapLoaded() {
+		mLoadingMapIndicator.mapFullyLoaded();
+	}
 }
