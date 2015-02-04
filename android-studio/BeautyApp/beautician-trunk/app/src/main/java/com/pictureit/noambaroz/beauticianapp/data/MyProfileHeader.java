5be5cd6d2081bffec5ca87a40bbb5478a6bea164
noambo <nbaroz@gmail.com>
2015-02-04 09:11:22 +0200

    remove unused objects

--- android-studio/BeautyApp/beautician-trunk/app/src/main/java/com/pictureit/noambaroz/beauticianapp/data/MyProfileHeader.java
+++
@@ -1,9 +0,0 @@
-package com.pictureit.noambaroz.beauticianapp.data;
-
-public class MyProfileHeader {
-
-	private String name;
-	private String image;
-	private String classification;
-	private String business_name;
-}
